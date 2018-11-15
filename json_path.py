524613677082d3fa5761d4bc97d01ad710bc3aa9
x2wing <x2wing@gmail.com>
2018-11-15 09:28:46 +0500

    yaml config was implemented

--- json_path.py
+++ json_path.py
@@ -1,5 +1,5 @@
 import json
-import yaml
+
 
 def write_to_config(config_path):
     with open(config_path, 'w', encoding='utf-8') as fd:
@@ -12,22 +12,13 @@ def read_from_config(config_path):
         json_d = json.load(fd)
         return json_d['file_paths']
 
-def read_file(path, mode='r', encoding='utf8'):
-    with open(path, mode) as fd:
-        for line in fd:
-            print(line, end='')
+
 
 
 if __name__ == '__main__':
     data = {'file_paths': [r'e:\temp\README.txt', r'e:\профиль\t.hdf5', r'e:\профиль\тест.hdf5']}
 
-    # Write YAML file
-    # with open('data.yaml', 'w', encoding='utf8') as outfile:
-    #     yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True)
-    #     # Read YAML file
-    with open("data.yaml", 'r', encoding='utf-8') as stream:
-        data_loaded = yaml.load(stream)
-        read_file(data_loaded['file_paths'][0], 'r', )
+
 
     # import os
     # # write_to_config('config.txt')
