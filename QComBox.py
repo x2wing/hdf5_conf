d95bc7179cd2e2ad5ddb27e7dba34a857ed1942e
x2wing <x2wing@gmail.com>
2018-11-16 17:00:09 +0500

    minor changes

--- QComBox.py
+++ QComBox.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import numpy as np
 from PyQt5.QtWidgets import (QWidget, QLabel,
                              QComboBox, QApplication, QPushButton, QFileDialog)
 from PyQt5 import QtCore
+
+from iof import Save, Load
+
+class Mysignal(QtCore.QObject):
+    mysignal = QtCore.pyqtSignal()
+
 
 
 class Example(QWidget):
@@ -12,20 +20,29 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.data = np.random.randint(1,10, (5,5))
+        print(self.data)
+        print(os.path.dirname(os.path.abspath(__file__)))
+
+        self.mysignal = Mysignal()
+
         self.lbl = QLabel("Ubuntu", self)
 
         self.combo = QComboBox(self)
         # data = {'file_paths': [r'e:\temp\README.txt', r'e:\профиль\t.hdf5', r'e:\профиль\тест.hdf5']}
         # self.combo.addItems(data['file_paths'])
         self.combo.setGeometry(0, 0, 200, 50)
-        btn_save = QPushButton("Save", self)
-        btn_save.setGeometry(0, 60, 60, 30)
-        btn_save.clicked[bool].connect(self.save_file)
+        self.btn_save = QPushButton("Save", self)
+        self.btn_save.setGeometry(0, 60, 60, 30)
+        self.btn_save.clicked[bool].connect(self.save_file)
 
         btn_load = QPushButton("Load", self)
         btn_load.setGeometry(70, 60, 60, 30)
-        btn_load.clicked[bool].connect(self.load_file)
+        # btn_load.clicked[bool].connect(self.load_file)
+        btn_load.clicked.connect(self.btn_save.click)
 
+
+        self.mysignal.mysignal.connect(self.save_file)
         # self.save_file()
 
         # self.combo.move(50, 50)
@@ -38,15 +55,22 @@ class Example(QWidget):
         self.setWindowFlags(QtCore.Qt.Widget|QtCore.Qt.MSWindowsFixedSizeDialogHint)
         self.show()
 
-    def save_file(self):
-        description = 'Таблица hdf5'
-        default_path = r'e:\temp'
+    def get_save_filepath(self):
+        description = 'Сохранить hdf5'
+        default_path = os.path.dirname(os.path.abspath(__file__))
         filter = "Таблицы hdf5  (*.hdf5)"
-        dialog = QFileDialog.getSaveFileName(self, description, default_path, filter)
-        self.combo.addItem(dialog[0])
+        path, _ = QFileDialog.getSaveFileName(self, description, default_path, filter)
+        print(path)
+        return path
+
+    def save_file(self):
+        path = self.get_save_filepath()
+        self.combo.addItem(path)
+        if path:
+            Save()(self.data, filepath=path)
 
     def load_file(self):
-        pass
+        self.mysignal.mysignal.emit()
 
     def set_item(self, pressed):
         self.combo.addItem('fucking code')
